0cf13dd3f6250f721b7a88f0201b9d8720bada3c
Alex P <apaskal@ucsd.edu>
2022-05-07 14:39:45 -0700

    added utils

---
+++ utils.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+
+
+def initialize(model_cls, *args, **kwargs):
+    """
+    Factory method for a neural network. Takes care of weight initialization, etc.
+    :param cls: the network class to be initialized
+    :return:
+    """
+
+    model = model_cls(*args, **kwargs)
+
+    for m in model.modules():
+        if isinstance(m, nn.Conv2d):
+            if m.bias is not None:
+                nn.init.uniform_(m.bias)
+            nn.init.xavier_uniform_(m.weight)
+
+        if isinstance(m, nn.ConvTranspose2d):
+            if m.bias is not None:
+                nn.init.uniform_(m.bias)
+            nn.init.xavier_uniform_(m.weight)
+
+    return model
