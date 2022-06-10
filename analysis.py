0c5edfcd29ea0b8a9a21ac587408b256ea6b022b
Alex P <apaskal@ucsd.edu>
2022-06-10 12:58:36 -0700

    added batch option

--- analysis.py
+++ analysis.py
@@ -4,10 +4,13 @@ from torch.utils.data import DataLoader
 import torch
 
 HardwareManager.use_gpu = True
-dataset = hd1k()
-model, info = load_model("flownet-ss-first-0607")
+# dataset = flying_chairs(split="val")
+dataset = sintel(pass_name="clean")
+# dataset = hd1k()
+# model, info = load_model("flownetSSwarped-0609")
+model, info = load_model("simple_s_test_new_epe")
 print("Dataset length:",len(dataset))
 dloader = DataLoader(dataset, batch_size=10)
 
 with torch.no_grad():
-    evaluate(dloader, model, verbose=True)
+    evaluate(dloader, model, verbose=True, batches=50)
